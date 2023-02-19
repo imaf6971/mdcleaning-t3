7b8585acd728ade51a3afb38ce46cc534f0d472a
Vladislav Mironov <imaf6971@gmail.com>
2023-02-19 21:48:39 +0300

    added api for multipart/form-data photo uploading

---
+++ apps/nextjs/src/pages/api/v1/cleanings/[id]/photos.ts
@@ -0,0 +1,51 @@
+import { prisma } from "@acme/db";
+import { NextApiHandler } from "next";
+import { parseForm } from "~/utils/parse-form";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const createCleaningPhotoInDb = async (
+  cleaningId: number,
+  filename: string,
+) => {
+  await prisma.cleaningPhotos.create({
+    data: {
+      path: filename,
+      actualCleaningId: cleaningId,
+    },
+  });
+};
+
+const handler: NextApiHandler = async (req, res) => {
+  if (req.method !== "POST") {
+    console.log("Photo upload is not a PATCH method");
+    res.setHeader("Allow", "POST");
+    res.status(405).json({
+      data: null,
+      error: "Method not allowed",
+    });
+    return;
+  }
+  const cleaningId = parseInt(req.query.id as string);
+  const { files } = await parseForm(req);
+  for (const fileName in files) {
+    const fileObj = files[fileName];
+    if (fileObj === undefined) {
+      continue;
+    }
+    if (Array.isArray(fileObj)) {
+      fileObj.forEach((file) => {
+        void createCleaningPhotoInDb(cleaningId, file.newFilename);
+      });
+      return;
+    }
+    void createCleaningPhotoInDb(cleaningId, fileObj.newFilename);
+  }
+  res.json({ ok: true });
+};
+
+export default handler;
