2811b033c7618de200b82bb7e575b1191e6b599b
Vladislav Mironov <imaf6971@gmail.com>
2023-02-21 11:54:21 +0300

    RoomTable is non editable now

--- apps/nextjs/src/components/room/RoomTable.tsx
+++ apps/nextjs/src/components/room/RoomTable.tsx
@@ -14,40 +14,16 @@ type RoomTableProps = {
 };
 
 export default function RoomTable({ rooms }: RoomTableProps) {
-  const utils = api.useContext();
-  const deleteRoom = api.rooms.deleteById.useMutation({
-    onSuccess() {
-      void utils.rooms.list.invalidate();
-    },
-  });
-  const [isEditing, setIsEditing] = useState(false);
   const [isAdding, setIsAdding] = useState(false);
 
   function roomLinks() {
     return rooms.map((room) => (
-      <RoomItem
-        isEditing={isEditing}
-        onRedo={() => {
-          // make ts happy
-        }}
-        onDelete={() => handleRoomDeleteClick(room.id)}
-        id={room.id}
-        title={room.title}
-        key={room.id}
-      />
+      <RoomItem id={room.id} title={room.title} key={room.id} />
     ));
-  }
-
-  function handleRoomDeleteClick(roomId: number) {
-    deleteRoom.mutate(roomId);
   }
 
   function handleAddRoomClick() {
     setIsAdding(true);
-  }
-
-  function handleEditRoomClick() {
-    setIsEditing((prev) => !prev);
   }
 
   return (
@@ -55,9 +31,10 @@ export default function RoomTable({ rooms }: RoomTableProps) {
       <BasicTable
         heading="1 этаж"
         items={roomLinks}
-        isEditing={isEditing}
+        // isEditing={isEditing}
+        editable={false}
         onAddClick={handleAddRoomClick}
-        onChangeClick={handleEditRoomClick}
+        // onChangeClick={handleEditRoomClick}
       />
       {isAdding && <AddRoomModal onClose={() => setIsAdding(false)} />}
     </>
