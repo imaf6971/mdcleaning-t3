06b84fd99dd1f8b341a0b2983c77ccba7f3643b4
Vladislav Mironov <imaf6971@gmail.com>
2023-02-21 11:55:14 +0300

    added disabled prop to button

--- apps/nextjs/src/ui/Button.tsx
+++ apps/nextjs/src/ui/Button.tsx
@@ -3,13 +3,19 @@ import type { MouseEventHandler, ReactNode } from "react";
 type ButtonProps = {
   onClick?: MouseEventHandler<HTMLButtonElement>;
   children: ReactNode;
+  disabled?: boolean;
 };
 
-export default function Button({ onClick, children }: ButtonProps) {
+export default function Button({
+  onClick,
+  children,
+  disabled = false,
+}: ButtonProps) {
   return (
     <button
       onClick={onClick}
-      className="rounded-md border p-2 transition-shadow hover:shadow focus:bg-gray-200 focus:outline-none focus:ring"
+      disabled={disabled}
+      className="disabled:bg-gray-200 rounded-md border p-2 transition-shadow hover:shadow focus:bg-gray-200 focus:outline-none focus:ring"
     >
       {children}
     </button>
