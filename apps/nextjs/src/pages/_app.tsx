39f1f3d9bfce7c80b4d7035fe765b6dfcb388b8f
Vladislav Mironov <imaf6971@gmail.com>
2023-03-02 17:11:57 +0300

    deleted ; char

--- apps/nextjs/src/pages/_app.tsx
+++ apps/nextjs/src/pages/_app.tsx
@@ -11,7 +11,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </SessionProvider>
   );
 };
