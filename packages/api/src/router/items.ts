b4b0ed23daee1ad7ce16b62c878ee2405275c289
Vladislav Mironov <imaf6971@gmail.com>
2023-03-03 12:07:27 +0300

    added 'add' procedure to item router

--- packages/api/src/router/items.ts
+++ packages/api/src/router/items.ts
@@ -1,9 +1,20 @@
+import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
 const item = createTRPCRouter({
-  list: publicProcedure.query(({ctx}) => {
+  list: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.item.findMany();
-  })
+  }),
+  add: publicProcedure
+    .input(z.object({
+      title: z.string().min(1),
+      description: z.string().min(1)
+    }))
+    .mutation(({ input, ctx }) => {
+      return ctx.prisma.item.create({
+        data: input
+      })
+    })
 })
 
 export default item;
