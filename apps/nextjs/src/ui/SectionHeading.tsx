72f7cc07f89479074f204cc155afe6cdfb6c476a
Vladislav Mironov <imaf6971@gmail.com>
2023-03-11 00:02:40 +0300

    added children prop to sectionheading

--- apps/nextjs/src/ui/SectionHeading.tsx
+++ apps/nextjs/src/ui/SectionHeading.tsx
@@ -1,8 +1,16 @@
-export default function SectionHeading({ heading }: { heading: string }) {
+import { ReactNode } from "react";
+
+type SectionHeadingProps = {
+  heading: string
+  children?: ReactNode
+}
+
+export default function SectionHeading({ children, heading }: SectionHeadingProps) {
   return (
     <div className="border-b p-3">
-      <div className="mx-auto md:w-2/3">
+      <div className="flex justify-between mx-auto md:w-2/3">
         <header className="text-2xl font-semibold">{heading}</header>
+        {children}
       </div>
     </div>
   );
