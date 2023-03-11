bc2e16d638b5db3c6f2bcbe5eaa8684dd9fa5b5f
Vladislav Mironov <imaf6971@gmail.com>
2023-03-11 13:47:11 +0300

    items adding implemented

--- apps/nextjs/src/pages/items/index.tsx
+++ apps/nextjs/src/pages/items/index.tsx
@@ -1,39 +1,121 @@
 import { useAutoAnimate } from "@formkit/auto-animate/react";
-import { NextPage } from "next"
+import { PlusCircleIcon } from "@heroicons/react/24/outline";
+import { NextPage } from "next";
 import Head from "next/head";
-import Navbar from "~/ui/Navbar"
+import { FormEventHandler, useState } from "react";
+import Button from "~/ui/Button";
+import Input from "~/ui/Input";
+import Modal from "~/ui/Modal";
+import Navbar from "~/ui/Navbar";
 import SectionHeading from "~/ui/SectionHeading";
 import Spinner from "~/ui/Spinner";
+import SubmitInput from "~/ui/SubmitInput";
 import { api } from "~/utils/api";
 
 const ItemsPage: NextPage = () => {
   const { data: items, isLoading, isError, isSuccess } = api.item.list.useQuery();
 
   const [itemList] = useAutoAnimate();
+  const [isAdding, setIsAdding] = useState(false);
   return (
     <>
       <Head>
         <title>Инвентарь</title>
       </Head>
       <Navbar />
-      <SectionHeading heading="Инвентарь" />
+      <SectionHeading heading="Инвентарь">
+        <Button onClick={() => setIsAdding(true)}>
+          <PlusCircleIcon className="h-5 w-5" />
+        </Button>
+      </SectionHeading>
       <main className="m-4 flex flex-col justify-center gap-2 md:mx-auto md:w-2/3">
         {isLoading && (
-          <div className="mx-auto">
+          <div className="w-full mx-auto">
             <Spinner />
           </div>
         )}
-        {isSuccess && <ul ref={itemList}>
-          {items.map((i) => (
-            <li key={i.id}>
-              {i.title}
-            </li>
-          ))}
-        </ul>}
+        {isSuccess && (
+          <ul className="flex flex-col gap-2" ref={itemList}>
+            {items.map((i) => (
+              <li
+                className="flex h-14 p-4 w-full justify-between rounded-md border transition-shadow hover:shadow"
+                key={i.id}>
+                {i.title}
+              </li>
+            ))}
+          </ul>
+        )}
         {isError && <span>Some error ocquired!!!</span>}
       </main>
+      {isAdding && <AddItemModal onClose={() => setIsAdding(false)} />}
     </>
   )
+}
+
+type AddItemModalProps = {
+  onClose: () => void
+}
+
+function AddItemModal({ onClose }: AddItemModalProps) {
+  const utils = api.useContext()
+  const addItem = api.item.add.useMutation();
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [img, setImg] = useState('');
+
+  const handleAddItem: FormEventHandler = (e) => {
+    e.preventDefault();
+    addItem.mutate({ title, description, img }, {
+      onSuccess() {
+        void utils.item.list.invalidate();
+      }
+    })
+  }
+  return (
+    <Modal onClose={onClose}>
+      <form onSubmit={handleAddItem} className="flex flex-col gap-2 p-2">
+        <h2 className="mb-2 text-lg font-medium">Добавить Предмет</h2>
+        <div>
+          <Input
+            id="itemTitle"
+            value={title}
+            label="Название"
+            onChange={(e) => setTitle(e.target.value)}
+            required
+            minLength={3}
+          />
+        </div>
+        <div>
+          <Input
+            id="itemDescription"
+            value={description}
+            label="Описание"
+            onChange={e => setDescription(e.target.value)}
+            required
+            minLength={3}
+          />
+        </div>
+        <input type='file' onChange={(e) => {
+          const file = e.target.files?.[0];
+          if (file === undefined) {
+            return;
+          }
+          blobToBase64(file)
+            .then((val) => setImg(val))
+            .catch(err => console.log(err))
+        }} />
+        <SubmitInput value="Добавить" />
+      </form>
+    </Modal>
+  )
+}
+
+function blobToBase64(blob: Blob) {
+  return new Promise<string>((res) => {
+    const reader = new FileReader();
+    reader.onloadend = () => res(reader.result as string)
+    reader.readAsDataURL(blob)
+  })
 }
 
 export default ItemsPage;
--- packages/api/src/router/items.ts
+++ packages/api/src/router/items.ts
@@ -1,5 +1,16 @@
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "../trpc";
+import { writeFile } from 'fs/promises'
+import { join } from 'path'
+
+export function getUploadDir(subdir: string) {
+  return join(
+    process.env.ROOT_DIR ?? process.cwd(),
+    `/public/uploads/${subdir}`
+  )
+}
+
+const ITEM_IMG_UPLOAD_DIR = getUploadDir('items');
 
 const item = createTRPCRouter({
   list: publicProcedure.query(({ ctx }) => {
@@ -8,13 +19,41 @@ const item = createTRPCRouter({
   add: publicProcedure
     .input(z.object({
       title: z.string().min(1),
-      description: z.string().min(1)
+      description: z.string().min(1),
+      img: z.string(),
     }))
-    .mutation(({ input, ctx }) => {
+    .mutation(async ({ input: { title, description, img }, ctx }) => {
+      const decodedImg = decodeBase64Image(img);
+
+      const FILE_NAME = `${Date.now()}.${decodedImg.type}`;
+      const photoPath = `${ITEM_IMG_UPLOAD_DIR}/${FILE_NAME}`;
+      await writeFile(photoPath, decodedImg.data)
+
       return ctx.prisma.item.create({
-        data: input
+        data: {
+          title,
+          description,
+          photoPath: FILE_NAME
+        }
       })
     })
 })
+
+function decodeBase64Image(data: string) {
+  const matched = data.match(/^data:([A-Za-z-+/]+);base64,(.+)$/)
+
+  if (matched?.length !== 3) {
+    throw new Error('Invalid input string')
+  }
+
+  const type = matched[1]?.match(/\/(.*?)$/)?.[1];
+  if (type === undefined) {
+    throw new Error('Oce pogano')
+  }
+  return {
+    type,
+    data: Buffer.from(matched[2] as string, 'base64'),
+  }
+}
 
 export default item;
